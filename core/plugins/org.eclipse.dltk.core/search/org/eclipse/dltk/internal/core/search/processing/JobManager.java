d7c1aa135e60082c148dd95ba70f1dac1ece6aaf
Kaloyan Raev <kaloyan.r@zend.com>
2013-08-13 15:09:10 +0300

    [414776] Severe performance Thread.sleep() bottleneck in JobManager
    Avoid sleeping 50 ms after each processed file
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=414776
    Signed-off-by: Kaloyan Raev <kaloyan.r@zend.com>

--- core/plugins/org.eclipse.dltk.core/search/org/eclipse/dltk/internal/core/search/processing/JobManager.java
+++ core/plugins/org.eclipse.dltk.core/search/org/eclipse/dltk/internal/core/search/processing/JobManager.java
@@ -471,8 +471,8 @@ public abstract class JobManager implements Runnable {
 						if (VERBOSE)
 							Util.verbose("FINISHED background job - " + job); //$NON-NLS-1$
 						moveToNextJob();
-						if (this.awaitingClients == 0)
-							Thread.sleep(50);
+						// if (this.awaitingClients == 0)
+						// Thread.sleep(50);
 					}
 				} catch (InterruptedException e) { // background indexing was
 					// interrupted
